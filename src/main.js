a3c2c759c4a4bb72f95cbdfb9aa76b717bfb1280
kary699 <1559396067@qq.com>
2020-03-01 11:34:37 +0800

    iview

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+Vue.use(ViewUI);
 import './style/index.scss'
 
 Vue.config.productionTip = false
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,14 +1,20 @@
-
 <template>
-  <div class="index">
-    <h1>This is index page</h1>
-  </div>
+<div> 
+    <Button>Default</Button>
+    <Button type="primary">Primary</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="text">Text</Button>
+    <br><br>
+    <Button type="info">Info</Button>
+    <Button type="success">Success</Button>
+    <Button type="warning">Warning</Button>
+    <Button type="error">Error</Button>
+    </div>
 </template>
 <script>
- import'../style/style.scss';
- export default {
-  
-}
+    export default {
+        
+    }
 </script>
 <style lang="scss"scoped>
     

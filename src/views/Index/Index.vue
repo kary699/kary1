a13cdb916578132ddecea17911c9aad443579dd7
安和 <1136913663@qq.com>
2020-03-03 11:51:11 +0800

    增加导航

---
+++ src/views/Index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="w-full h-main" @click="ifShowSearch">
+		<Header ref="header" />
+		<div class="min-w-1200 h-full flex j-between">
+			index
+		</div>
+	</div>
+</template>
+
+<script>
+import Header from './components/Header/Header'
+export default {
+	components: {
+		Header
+	}
+}
+</script>
---
+++ src/views/Index/components/Header/components/User.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="absolute top-30 w-100 user-css header-panel-css shadow-grey bg-white b-r-5 m-t-20 text-center fs-12 text-grey p-h-10 zIndex-9" :class="{'header-panel-show': ifShow}">
+        <div class="cursor-pointer p-10 b-b">{{name}}</div>
+        <div class="cursor-pointer p-10 b-b">组织管理</div>
+        <div class="cursor-pointer p-10 b-b">审批/请假</div>
+        <div class="cursor-pointer p-10 b-b">考核测试</div>
+        <div class="cursor-pointer p-10 b-b">项目空间</div>
+        <router-link to="/sign" class="d-block cursor-pointer p-10">
+            <span class="text-red">退出账号</span>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            ifShow: false,
+            name: '安和'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.user-css {
+    left: -50px;
+}
+</style>

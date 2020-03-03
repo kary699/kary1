3968bc01208ac9f7dad80a862182d59bbda2718c
安和 <1136913663@qq.com>
2020-03-03 19:28:30 +0800

    增加聊天模块

--- src/views/Index/Index.vue
+++ src/views/Index/Index.vue
@@ -4,14 +4,17 @@
 		<div class="min-w-1200 h-full flex j-between">
 			index
 		</div>
+		<Chat />
 	</div>
 </template>
 
 <script>
 import Header from './components/Header/Header'
+import Chat from './components/Chat/Chat'
 export default {
 	components: {
-		Header
+		Header,
+		Chat
 	}
 }
 </script>

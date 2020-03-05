3a3a91222daeb95dfc5f5f744c12ced1642176f6
kary699 <1559396067@qq.com>
2020-03-05 16:55:51 +0800

    leave

--- src/views/Work/Work.vue
+++ src/views/Work/Work.vue
@@ -1,19 +1,31 @@
 <template>
-    <div w-full h-full>
-        <Leave/>
-        <Write/>
-        <List/>       
-    </div>  
+      <div class=" w-full h-full flex j-center a-center absolute"> 
+             <Write ref="write"/>
+             <List/>
+         <div class="w-200 h-200 m-v-90 absolute">
+            <Button type="info" size="large" ghost class="m-t-80"> 审批</Button>
+            <Button type="info" size="large" ghost  @click="isShow()">请假</Button>
+        </div>
+    </div>             
 </template>
 <script>
-import Write from './components/Write'
-import Leave from './components/Leave'
+import Write from './components/write/Write'
 import List from './components/List'
 export default {
     components: {
         Write,
-        Leave,
         List
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        isShow(){
+            this.$refs.write.showWrite()
+
+        }
     }
     
 }
